11f01575c223152c81fe3fb4f329632176d16c1f
Victor Alonso <victoralonso@macbook-pro-de-victor-2.local>
2018-11-22 22:32:27 -0500

    Initial Commit

---
+++ ChristmasApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChristmasApp
+//
+//  Created by Victor Alonso on 2018-11-22.
+//  Copyright © 2018 Victor Alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
