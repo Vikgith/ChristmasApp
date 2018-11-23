7d8b3c7cc1ccb5ad0de6a43d0c392596ab3fea93
Victor Alonso <victoralonso@macbook-pro-de-victor-2.local>
2018-11-23 14:48:52 -0500

    Testing the flip option

--- ChristmasApp/Controller/ScratchCardViewController.swift
+++ ChristmasApp/Controller/ScratchCardViewController.swift
@@ -13,29 +13,46 @@ import SRScratchView
 
 class ViewController: UIViewController,SRScratchViewDelegate{
  
+    
+    private var isFlipped: Bool = false
+    
+    
     @IBOutlet weak var backImageView: UIImageView!
     @IBOutlet weak var frontImageView: UIImageView!
     @IBOutlet weak var scratchCardView: UIView!
     @IBOutlet weak var scratchImageView: SRScratchView!
+    
     @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var backCardView: UIView!
+    
+    
     
     @IBAction func snowButton(_ sender: UIButton) {
         //createParticles(color: UIColor.white, color2: UIColor.white, color3: UIColor.white)
+        
+        isFlipped = !isFlipped
+        
+        let cardToFlip = isFlipped ? cardView : backCardView
+        let bottomCard = isFlipped ? backCardView : cardView
+        
+        UIView.transition(from: cardToFlip!,
+                          to: bottomCard!,
+                          duration: 0.5,
+                          options: [.transitionFlipFromRight, .showHideTransitionViews],
+                          completion: nil)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        backImageView.isHidden = true
-        
         //width of srcatch line
         scratchImageView.lineWidth = 30
-        
+        //Set delegate to the sratchController
         self.scratchImageView.delegate = self
-        
+        //Make scratch surface a circle
         self.scratchImageView.layer.cornerRadius = self.scratchImageView.frame.height/2
         self.scratchImageView.layer.masksToBounds = true
-        
         self.scratchCardView.layer.cornerRadius = self.scratchImageView.frame.height/2
         self.scratchCardView.layer.masksToBounds = true
         
