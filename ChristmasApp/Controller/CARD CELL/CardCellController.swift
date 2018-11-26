3799dde97364f5c868c4768a534b1fa4e3fea9fe
Victor Alonso <victoralonso@macbook-pro-de-victor-2.local>
2018-11-26 14:34:36 -0500

    Commit before important implementations

--- ChristmasApp/Controller/CARD CELL/CardCellController.swift
+++ ChristmasApp/Controller/CARD CELL/CardCellController.swift
@@ -21,21 +21,9 @@ class CardCellController: CardCell {
         let randomRed:CGFloat = CGFloat(arc4random()) / CGFloat(UInt32.max)
         let randomGreen:CGFloat = CGFloat(arc4random()) / CGFloat(UInt32.max)
         let randomBlue:CGFloat = CGFloat(arc4random()) / CGFloat(UInt32.max)
-        
         self.backgroundColor = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
-    }
-    
-
-    override func prepareForReuse() {
-        super.prepareForReuse()
         
-    }
-    
-    override func layoutSubviews() {
-        
-        self.layer.cornerRadius = 12
-        
-        super.layoutSubviews()
+        self.layer.cornerRadius = 30
     }
 
 }
