b81e14128609e0e3045e3977ea26d03d68a0bb5d
Victor Alonso <victoralonso@macbook-pro-de-victor-2.local>
2018-11-28 15:21:36 -0500

    Perform changes

--- ChristmasApp/Controller/ScratchCardViewController.swift
+++ ChristmasApp/Controller/ScratchCardViewController.swift
@@ -12,37 +12,34 @@ import SRScratchView
 @IBDesignable
 
 class ViewController: UIViewController,SRScratchViewDelegate{
- 
     
+    //variables & Let
     private var isFlipped: Bool = false
     
-    
-    @IBOutlet weak var scratchCardView: UIView!
+    //Interface Builders
+    @IBOutlet weak var winImage: UIImageView!
     @IBOutlet weak var scratchImageView: SRScratchView!
     
-    
-    
-    @IBOutlet weak var cardView: UIView! //FrontView (View:scratch,images,..)
-    @IBOutlet weak var backCardView: UIView! //CardView (View: front&back
-    
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var frontView: UIView!
     @IBOutlet weak var backView: UIView!
-    
     
     @IBAction func snowButton(_ sender: UIButton) {
         
         isFlipped = !isFlipped
         
-        let cardToFlip = isFlipped ? backView : cardView //cardView : backImageView
-        let bottomCard = isFlipped ? cardView : backView //backImageView : cardView
+        let cardToFlip = isFlipped ? backView : frontView
+        let bottomCard = isFlipped ? frontView : backView
         
         UIView.transition(from: cardToFlip!,
                           to: bottomCard!,
-                          duration: 0.5,
+                          duration: 1,
                           options: [.transitionFlipFromRight, .showHideTransitionViews],
                           completion: nil)
         
     }
     
+    //View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -50,29 +47,35 @@ class ViewController: UIViewController,SRScratchViewDelegate{
         scratchImageView.lineWidth = 30
         //Set delegate to the sratchController
         self.scratchImageView.delegate = self
-        //Make scratch surface a circle
-        self.scratchImageView.layer.cornerRadius = self.scratchImageView.frame.height/2
-        self.scratchImageView.layer.masksToBounds = true
-        self.scratchCardView.layer.cornerRadius = self.scratchImageView.frame.height/2
-        self.scratchCardView.layer.masksToBounds = true
-    
-        self.cardView.cornerRadius = 50
-        self.cardView.layer.masksToBounds = true
-        
+        //Set the corner radius of the views
+        setCorners()
         
     }
     
-    
+    //Function of SRScratchViewDelegate for set the "eraseProgress"
     func scratchCardEraseProgress(eraseProgress: Float) {
         print(eraseProgress)
         
-        if eraseProgress > 50.0{
+        if eraseProgress > 40.0{
             UIView.animate(withDuration: 0.5, animations: {
                 self.scratchImageView.alpha = 0.0
-                self.backCardView.boing()
+                self.cardView.boing()
                 self.createParticles(color: UIColor.white, color2: UIColor.white, color3: UIColor.white)
             })
         }
+    }
+    
+    func setCorners(){
+        
+        //Make scratch surface a circle
+        self.scratchImageView.layer.cornerRadius = self.scratchImageView.frame.height/2
+        self.scratchImageView.layer.masksToBounds = true
+        self.winImage.layer.cornerRadius = self.scratchImageView.frame.height/2
+        self.winImage.layer.masksToBounds = true
+        
+        self.frontView.cornerRadius = 50
+        self.frontView.layer.masksToBounds = true
+        
     }
     
 }
