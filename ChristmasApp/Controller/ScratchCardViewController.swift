7e59c65e6ba2a33a5a67516c854840cd528d7a7a
Victor Alonso <victoralonso@macbook-pro-de-victor-2.local>
2018-11-30 14:28:29 -0500

    selected Card rotate 360º and zoom for scratch

--- ChristmasApp/Controller/ScratchCardViewController.swift
+++ ChristmasApp/Controller/ScratchCardViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController,SRScratchViewDelegate{
     //MARK: VARIABLES AND LET
     private var isFlipped: Bool = false
     var num = 0
-    var animator: UIViewPropertyAnimator!
+    var animator1: UIViewPropertyAnimator!
+    var animator2: UIViewPropertyAnimator!
+    var animator3: UIViewPropertyAnimator!
+    var animator4: UIViewPropertyAnimator!
+
     
     //MARK: OUTLETS ///////////////////////////////////////////////////////
     
@@ -60,39 +64,60 @@ class ViewController: UIViewController,SRScratchViewDelegate{
         if sender.tag==1 {
             
             num = 1
-            animator = UIViewPropertyAnimator(duration: 2, curve: .linear) {/* [unowned self, generalView] in*/
-                self.cardView1!.center.x = self.generalView.center.x
-                self.cardView1!.center.y = self.generalView.center.y
-                
-//                cardView1!.center.x = 187.5
-//                cardView1!.center.y = 333.5
-//                cardView1!.transform = CGAffineTransform(rotationAngle: CGFloat.pi).scaledBy(x: 0.001, y: 0.001)
-            }
-            animator.startAnimation()
+            animator1.startAnimation()
             //flipCard(view1: backView1, view2: frontView1)
+            UIView.animate(withDuration:1, animations: {
+                self.cardView2.alpha=0
+                self.cardView3.alpha=0
+                self.cardView4.alpha=0
+            })
+            
+            
         } else if sender.tag==2 {
+            
             num=2
-            flipCard(view1: backView2, view2: frontView2)
+            animator2.startAnimation()
+            //flipCard(view1: backView2, view2: frontView2)
+            UIView.animate(withDuration:1, animations: {
+                self.cardView1.alpha=0
+                self.cardView3.alpha=0
+                self.cardView4.alpha=0
+            })
+            
         } else if sender.tag==3 {
+            
             num=3
-            flipCard(view1: backView3, view2: frontView3)
+            animator3.startAnimation()
+            //flipCard(view1: backView3, view2: frontView3)
+            UIView.animate(withDuration:1, animations: {
+                self.cardView2.alpha=0
+                self.cardView1.alpha=0
+                self.cardView4.alpha=0
+            })
+            
         } else if sender.tag==4 {
+            
             num=4
-            flipCard(view1: backView4, view2: frontView4)
+            animator4.startAnimation()
+            //flipCard(view1: backView4, view2: frontView4)
+            UIView.animate(withDuration:1, animations: {
+                self.cardView2.alpha=0
+                self.cardView3.alpha=0
+                self.cardView1.alpha=0
+            })
+            
         }
+        
+        
     }
     
     //MARK: VIEW DID LOAD //////////////////////////////////////////////////
     override func viewDidLoad() { super.viewDidLoad()
         
-        cardView1.translatesAutoresizingMaskIntoConstraints = false
-        
         setLayers()
-        self.scratchImage1.delegate = self
-        self.scratchImage2.delegate = self
-        self.scratchImage3.delegate = self
-        self.scratchImage4.delegate = self
-        scratchImage2.lineWidth = 20
+        setAnimations()
+        setDelegatesAndLineWidth()
+    
         
     }
     
@@ -103,23 +128,23 @@ class ViewController: UIViewController,SRScratchViewDelegate{
         
         print(eraseProgress)
         
-        if eraseProgress > 50{
+        if eraseProgress > 40{
             UIView.animate(withDuration: 0.5, animations: {
                 if self.num == 1{
                     self.scratchImage1.alpha = 0.0
-                    self.cardView1.boing()
+                    //self.cardView1.boing()
                     self.createParticles()
                 }else if self.num == 2{
                     self.scratchImage2.alpha = 0.0
-                    self.cardView2.boing()
+                    //self.cardView2.boing()
                     self.createParticles()
                 }else if self.num == 3{
                     self.scratchImage3.alpha = 0.0
-                    self.cardView3.boing()
+                    //self.cardView3.boing()
                     self.createParticles()
                 }else if self.num == 4{
                     self.scratchImage4.alpha = 0.0
-                    self.cardView4.boing()
+                    //self.cardView4.boing()
                     self.createParticles()
                 }
                 
@@ -137,8 +162,9 @@ class ViewController: UIViewController,SRScratchViewDelegate{
         
         UIView.transition(from: cardToFlip!,
                           to: bottomCard!,
-                          duration: 0.5,
-                          options: [.transitionFlipFromRight, .showHideTransitionViews],
+                          duration: 2,
+                          options: [.transitionFlipFromLeft, .showHideTransitionViews,
+                                    .preferredFramesPerSecond60, .curveEaseInOut ],
                           completion: nil)
     }
     
@@ -160,14 +186,12 @@ class ViewController: UIViewController,SRScratchViewDelegate{
         self.frontView2.cornerRadius = 10
         self.frontView2.layer.masksToBounds = true
 
-        
         self.scratchImage3.layer.cornerRadius = self.scratchImage2.frame.height/2
         self.scratchImage3.layer.masksToBounds = true
         self.winImage3.layer.cornerRadius = self.scratchImage2.frame.height/2
         self.winImage3.layer.masksToBounds = true
         self.frontView3.cornerRadius = 10
         self.frontView3.layer.masksToBounds = true
-
         
         self.scratchImage4.layer.cornerRadius = self.scratchImage2.frame.height/2
         self.scratchImage4.layer.masksToBounds = true
@@ -177,6 +201,54 @@ class ViewController: UIViewController,SRScratchViewDelegate{
         self.frontView4.layer.masksToBounds = true
 
     }
+    
+    func setAnimations(){
+        
+        animator1 = UIViewPropertyAnimator(duration: 1.5, curve: .easeInOut) {/* [unowned self, box] in*/
+            self.cardView1.center.x = self.generalView.center.x
+            self.cardView1.center.y = self.generalView.center.y
+            
+            self.cardView1.transform = CGAffineTransform(rotationAngle: CGFloat.pi).scaledBy(x: 3.5, y: 3.5)
+            self.cardView1.transform = CGAffineTransform(rotationAngle: CGFloat.pi*2).scaledBy(x: 3.5, y: 3.5)
+        }
+        
+        animator2 = UIViewPropertyAnimator(duration: 1.5, curve: .easeInOut) {
+            self.cardView2.center.x = self.generalView.center.x
+            self.cardView2.center.y = self.generalView.center.y
+            
+            self.cardView2.transform = CGAffineTransform(rotationAngle: CGFloat.pi).scaledBy(x: 3.5, y: 3.5)
+            self.cardView2.transform = CGAffineTransform(rotationAngle: CGFloat.pi*2).scaledBy(x: 3.5, y: 3.5)
+        }
+        
+        animator3 = UIViewPropertyAnimator(duration: 1.5, curve: .easeInOut) {
+            self.cardView3.center.x = self.generalView.center.x
+            self.cardView3.center.y = self.generalView.center.y
+            
+            self.cardView3.transform = CGAffineTransform(rotationAngle: CGFloat.pi).scaledBy(x: 3.5, y: 3.5)
+            self.cardView3.transform = CGAffineTransform(rotationAngle: CGFloat.pi*2).scaledBy(x: 3.5, y: 3.5)
+        }
+        
+        animator4 = UIViewPropertyAnimator(duration: 1.5, curve: .easeInOut) {
+            self.cardView4.center.x = self.generalView.center.x
+            self.cardView4.center.y = self.generalView.center.y
+            
+            self.cardView4.transform = CGAffineTransform(rotationAngle: CGFloat.pi).scaledBy(x: 3.5, y: 3.5)
+            self.cardView4.transform = CGAffineTransform(rotationAngle: CGFloat.pi*2).scaledBy(x: 3.5, y: 3.5)
+        }
+    }
+    
+    func setDelegatesAndLineWidth (){
+        self.scratchImage1.delegate = self
+        self.scratchImage2.delegate = self
+        self.scratchImage3.delegate = self
+        self.scratchImage4.delegate = self
+        
+        scratchImage1.lineWidth = 10
+        scratchImage2.lineWidth = 10
+        scratchImage3.lineWidth = 10
+        scratchImage4.lineWidth = 10
+    }
+    
     
 }
 
